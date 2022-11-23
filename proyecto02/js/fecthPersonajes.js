d01f85d407f4f39a3ccf1eac2005e10b2dfc59e2
GilsonPonce <giljeffpobi@gmail.com>
2022-11-23 15:22:26 -0500

    paginacion

--- proyecto02/js/fecthPersonajes.js
+++ proyecto02/js/fecthPersonajes.js
@@ -1,9 +1,38 @@
+const btnnext = document.getElementById("btnnext");
+const btnprevious = document.getElementById("btnprevious");
+const limit = 4;
+let peticion = 0;
+
+
 window.addEventListener("load", () => {
-    const containerPersonajes = document.querySelector("article.container section#personajes");
+  llenarPersonajes();
+  habilitarBotonPrevious();
+});
+
+btnnext.addEventListener("click", () => {
+  peticion++;
+  let offset = peticion * limit;
+  llenarPersonajes(offset);
+  habilitarBotonPrevious();
+});
+
+btnprevious.addEventListener("click", () => {
+  peticion--;
+  let offset = peticion * limit;
+  llenarPersonajes(offset);
+  habilitarBotonPrevious();
+});
+
+function llenarPersonajes(offset) {
+  const containerPersonajes = document.querySelector("article.container section#personajes");
+  containerPersonajes.innerHTML = `<div class="spinner-border" style="width: 20rem; height: 20rem;" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>`;
+  obtenerPersonajes(offset).then(({ results }) => {
     containerPersonajes.innerHTML = "";
-    obtenerPersonajes().then(({ results }) => {
-        results.map(({ name, modified, description, thumbnail }) => {
-            containerPersonajes.innerHTML += `
+    results.map(({ name, description, thumbnail }) => {
+
+      containerPersonajes.innerHTML += `
             <div class="col-6">
             <div class="card mb-3" style="max-width: 540px;">
             <div class="row g-0">
@@ -13,21 +42,28 @@ window.addEventListener("load", () => {
               <div class="col-md-8">
                 <div class="card-body text-dark">
                   <h5 class="card-title">${name}</h5>
-                  <p class="card-text fs-6">${description}</p>
-                  <p class="card-text"><small class="text-muted">${modified}</small></p>
+                  <p class="card-text">${description == "" ? "No description to show" : description}</p>
                 </div>
               </div>
             </div>
           </div>
         </div>`
-        })
+
     })
+  })
+}
 
-});
+async function obtenerPersonajes(offset = 0) {
+  const URL = `http://gateway.marvel.com/v1/public/characters?limit=${limit}&offset=${offset}&ts=3000&apikey=c0c7a7a4d7b567db6bdd487b26f69959&hash=edd7c232f6dda15da4de0ba9ce76426d`;
+  const response = await fetch(URL);
+  const personajes = await response.json();
+  return personajes.data;
+}
 
-async function obtenerPersonajes(limit = 6, offset = 0) {
-    const URL = `http://gateway.marvel.com/v1/public/characters?limit=${limit}&offset=${offset}&ts=3000&apikey=c0c7a7a4d7b567db6bdd487b26f69959&hash=edd7c232f6dda15da4de0ba9ce76426d`;
-    const response = await fetch(URL);
-    const personajes = await response.json();
-    return personajes.data;
+function habilitarBotonPrevious() {
+  if (peticion != 0) {
+    btnprevious.classList.remove("d-none")
+  } else {
+    btnprevious.classList.add("d-none")
+  }
 }
