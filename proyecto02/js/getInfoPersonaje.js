699558df6b2ae8c8495e0d5d5bfd2f240a018e4e
GilsonPonce <giljeffpobi@gmail.com>
2022-11-25 22:30:58 -0500

    filtros de datos y diseño

--- proyecto02/js/getInfoPersonaje.js
+++ proyecto02/js/getInfoPersonaje.js
@@ -17,33 +17,49 @@ function cargarInterfaz(urlCharacter) {
         }
         let { path, extension } = thumbnail;
         let articleMain = document.querySelector("article.container");
-        articleMain.innerHTML = `<section class="row justify-content-center"><div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src="${path+"."+extension}" class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">${name}</h5>
-              <p class="card-text">${description}</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        articleMain.innerHTML = `<section class="row justify-content-center align-items-center">
+        <div class="col-6">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${path+"."+extension}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${name}</h5>
+                            <p class="card-text">${description}</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Open this select menu</option>
+                <option value="1">Comics</option>
+                <option value="2">Series</option>
+                <option value="3">Stories</option>
+                <option value="3">Events</option>
+            </select>
         </div>
-      </div></section>
-      <section class="row"><canvas id="graficone"></canvas></section>`
+        <div class="col-5 text-center">
+            <canvas id="graficone"></canvas>
+        </div>
+        <div class="col-6 text-center">
+            <canvas id="grafictwo"></canvas>
+        </div>
+      </section>
+      <section class="row justify-content-center m-3"><button class="btn btn-danger col-3" onclick="window.location.reload()">Return</button></section>`
       setTimeout(cargarGraficaOne(arrNumeros),1000)
     });
 }
 
 function cargarGraficaOne({comics,series,stories,events}){
     const grafi = document.querySelector("section.row canvas#graficone");
-    console.log(grafi)
     let confi = {
         type:'doughnut',
         data: {
             labels:[
-                'comic',
+                'comics',
                 'series',
                 'stories',
                 'events'
@@ -62,6 +78,10 @@ function cargarGraficaOne({comics,series,stories,events}){
         options:{}
     }
     new Chart(grafi,confi)
+}
+
+function cargarGraficaTwo(filter="1"){
+
 }
 
 
